72d7ccd33b52db9164b52dc259325cd53123ef9f
Nishant Gupta <shub002gupta@gmail.com>
2020-05-12 22:12:25 +0530

    Initial Commit

---
+++ ImageViewerApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageViewerApp
+//
+//  Created by Nishant Gupta on 12/05/20.
+//  Copyright © 2020 Nishant Gupta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
